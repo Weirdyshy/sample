4ef2120ecc24138c66962f616c9ae7d5a885389e
Weirdyshy <shybae19@gmail.com>
2018-11-20 11:22:48 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="lib/font-awesome/css/font-awesome.min.css">
+    <script src="main.js"></script>
+    
+    
+</head>
+<body>
+    <div class="containter">
+        <div id="nav_bar">
+            <a class="logo" rel="index" href="index.html"> </a>
+            <img src="/img/me.jpg" alt="logo" />
+            <img src="/img/me.jpg" alt="logo"/>
+            <nav class="en">
+                <a rel="index" href="index.html" class="home-link active"> <i class="fa fa-fw fa-home" aria-hidden="true"></i> </a>  
+                <a href="#about" class=""><i class="fa fa-fw fa-user" aria-hidden="true"></i></a>
+                <a href="#skills" class=""><i class="fa fa-fw fa-wrench" aria-hidden="true"></i> </a>
+                <a href="#mywork" class=""><i class="fa fa-briefcase" aria-hidden="true"></i></a>
+                <a href="#contact" class=""><i class="fa fa-fw fa-envelope" aria-hidden="true"></i> </a>
+            </nav>
+
+            <ul>
+                <li><a href="htttp://facebook.com/shyracz" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="htttp://facebook.com/shyracz" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="htttp://facebook.com/shyracz" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="htttp://facebook.com/shyracz" target="_blank"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="htttp://facebook.com/shyracz" target="_blank"><i class="fa fa-twitch"></i></a></li>
+            </ul>
+           
+        </div>
+
+ 
+    </div>
+    <div id="page" class="en">
+        <div class="container home-page" style="opacity: 1; transform:matrix(1,0,0,1,0,0);">
+            <span class="tags top-tags">&nbsp;&nbsp;&nbsp;<body></span>
+            <div class="text-zone">
+            <span class="tags bottom-tags">&nbsp;&nbsp;&nbsp;<?php echo "</body>"?>;
+                <br>
+                " </html> " </span>
+
+                
+               <h1 aria-label="Hi, I'm Shyra, Web Developer." class="blast-root">
+                   <span class="blast" aria-hidden="true" style="opacity:1;">H</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">i</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">,</span>
+                   <br>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">I</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">'</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">m</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">S</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">h</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">y</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">r</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">a</span>
+                   <br>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">w</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">e</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">b</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">&nbsp;</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">d</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">e</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">v</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">e</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">l</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">o</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">p</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">e</span>
+                   <span class="blast" aria-hidden="true" style="opacity:1;">r</span>
+               </h1>
+               <h2>Web Developer | Programmer | Gamer</h2>
+               <a rel="contact" href="#" class="flat-button">CONTACT ME</a>
+            </div>
+        
+            <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+
+
+    </div>    
+        
+    </div>
+
+    
+    
+ <!-- JavaScript Libraries -->
+ <script src="lib/jquery/jquery.min.js"></script>
+ <script src="lib/jquery/jquery-migrate.min.js"></script>
+ <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="js/home.js"></script>
+
+</body>
+</html>
